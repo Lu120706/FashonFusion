fb873b2957a7849859ec19367a3db44e407e5a40
1kevin6 <morenolcastillo@gmail.com>
2025-09-29 19:30:38 -0500

    Dependecia agregada

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import base64
 import os
 from flask import Flask, render_template, request, redirect, url_for, session, flash, abort, send_from_directory, jsonify
 from werkzeug.utils import secure_filename
